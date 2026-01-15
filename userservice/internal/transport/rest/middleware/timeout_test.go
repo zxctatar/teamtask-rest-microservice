97427ae755b41181427bca9228fb4019027156c0
ashlyaud <maksimknutov90@gmail.com>
2026-01-16 00:02:48 +0300

    added table tests for timeout middleware

---
+++ userservice/internal/transport/rest/middleware/timeout_test.go
@@ -0,0 +1,59 @@
+package middleware
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TimeoutTest(t *testing.T) {
+	tests := []struct {
+		name   string
+		timeMs time.Duration
+		status int
+		body   string
+	}{
+		{
+			name:   "good test",
+			timeMs: 1 * time.Millisecond,
+			status: http.StatusOK,
+			body:   "ok",
+		},
+		{
+			name:   "timeout test",
+			timeMs: 3 * time.Millisecond,
+			status: http.StatusRequestTimeout,
+			body:   "timeout",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := gin.New()
+			r.Use(TimeoutMiddleware(tt.timeMs))
+
+			r.GET("/test", func(ctx *gin.Context) {
+				time.Sleep(2 * time.Millisecond)
+				ctx.JSON(http.StatusOK, "ok")
+			})
+
+			serv := httptest.NewServer(r)
+			defer serv.Close()
+
+			resp, err := http.Get(serv.URL + "/test")
+			assert.NoError(t, err)
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.body, body)
+			assert.Equal(t, tt.status, resp.StatusCode)
+		})
+	}
+}
