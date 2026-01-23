4db5d282772b42ef2eb32874c6f7966ba601d6cf
ashlyaud <maksimknutov90@gmail.com>
2026-01-23 19:28:19 +0300

    feat: added new func Get

--- userservice/internal/infrastructure/redis/redis.go
+++ userservice/internal/infrastructure/redis/redis.go
@@ -2,9 +2,15 @@ package myredis
 
 import (
 	"context"
+	"errors"
 	"time"
+	"userservice/internal/repository/session"
 
 	"github.com/redis/go-redis/v9"
+)
+
+var (
+	invalidId uint32 = 0
 )
 
 type Redis struct {
@@ -21,4 +27,15 @@ func NewRedis(client *redis.Client, ttl *time.Duration) *Redis {
 
 func (r *Redis) Save(ctx context.Context, sessionId string, userId uint32) error {
 	return r.client.Set(ctx, sessionId, userId, *r.ttl).Err()
+}
+
+func (r *Redis) Get(ctx context.Context, sessionId string) (uint32, error) {
+	id, err := r.client.Get(ctx, sessionId).Int()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return invalidId, session.ErrKeyNotFound
+		}
+		return invalidId, err
+	}
+	return uint32(id), nil
 }
