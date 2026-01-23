d5b7d545af0853db97eaec0e748cd59e0432e4c7
ashlyaud <maksimknutov90@gmail.com>
2026-01-23 23:07:07 +0300

    feat: added implementation for GetIdBySession function in handler

--- userservice/internal/transport/grpc/handler/grpchandler.go
+++ userservice/internal/transport/grpc/handler/grpchandler.go
@@ -2,21 +2,60 @@ package grpchandler
 
 import (
 	"context"
+	"errors"
 	"log/slog"
+	"time"
+	autherr "userservice/internal/usecase/errors/authenticate"
+	"userservice/internal/usecase/interfaces"
+	authmodel "userservice/internal/usecase/models/authenticate"
 	userservicev1 "userservice/proto/userservice"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type GRPCHandler struct {
-	log *slog.Logger
+	log     *slog.Logger
+	timeout time.Duration
 	userservicev1.UnimplementedUserServiceServer
+
+	authUC interfaces.AuthenticateUsecase
 }
 
-func NewGRPCHandler(log *slog.Logger) *GRPCHandler {
+func NewGRPCHandler(log *slog.Logger, timeout time.Duration, authUC interfaces.AuthenticateUsecase) *GRPCHandler {
 	return &GRPCHandler{
-		log: log,
+		log:     log,
+		timeout: timeout,
+		authUC:  authUC,
 	}
 }
 
 func (g *GRPCHandler) GetIdBySession(ctx context.Context, req *userservicev1.GetIdBySessionRequest) (*userservicev1.GetIdBySessionResponse, error) {
-	panic("not implemented")
+	const op = "grpchandler.GetIdBySession"
+	log := g.log.With(slog.String("op", op))
+
+	log.Info("start get id by session request")
+
+	ctx, cancel := context.WithTimeout(ctx, g.timeout)
+	defer cancel()
+
+	in := authmodel.NewAuthInput(req.SessionId)
+
+	out, err := g.authUC.AuthenticateSession(ctx, in)
+	if ctx.Err() != nil {
+		log.Warn("timeout")
+		return nil, status.Error(codes.DeadlineExceeded, "request time out")
+	}
+	if err != nil {
+		if errors.Is(err, autherr.ErrSessionNotFound) {
+			log.Info("session not found")
+			return nil, status.Error(codes.NotFound, "session not found")
+		}
+		log.Warn("cannotfailed to get user id", slog.String("error", err.Error()))
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	return &userservicev1.GetIdBySessionResponse{
+		UserId: out.UserId,
+	}, nil
 }
