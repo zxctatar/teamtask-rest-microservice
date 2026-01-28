802e98dfde9ab71836da91c811dd9f6cc626146b
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-29 01:38:49 +0300

    chore: connection to the database is created when the service starts

--- projectservice/internal/app/app.go
+++ projectservice/internal/app/app.go
@@ -2,10 +2,14 @@ package app
 
 import (
 	"context"
+	"database/sql"
 	"log/slog"
 	"projectservice/internal/config"
 	userserviceclient "projectservice/internal/infrastructure/grpc/userservice"
+	"projectservice/internal/infrastructure/postgres"
 	"projectservice/internal/transport/rest"
+	resthandler "projectservice/internal/transport/rest/handler"
+	"projectservice/internal/usecase/implementations/createproject"
 	"projectservice/pkg/logger"
 )
 
@@ -13,20 +17,28 @@ type App struct {
 	log  *slog.Logger
 	cfg  *config.Config
 	serv *rest.RestServer
+	db   *sql.DB
 }
 
 func NewApp() *App {
 	cfg := config.MustLoad()
 	log := logger.SetupLogger(cfg.LoggerConf.Level)
+	db := mustLoadPostgres(cfg)
 
 	sessionValid := userserviceclient.NewUserServiceClient(log, cfg.ConnectionsConf.UserServConnConf.Host, cfg.ConnectionsConf.UserServConnConf.Port)
+	postgres := postgres.NewPostgres(db)
 
-	serv := mustLoadHttpServer(cfg, log, sessionValid)
+	createProjectUC := createproject.NewCreateProject(log, postgres)
+
+	handl := resthandler.NewHandler(log, createProjectUC)
+
+	serv := mustLoadHttpServer(cfg, log, handl, sessionValid)
 
 	return &App{
 		log:  log,
 		cfg:  cfg,
 		serv: serv,
+		db:   db,
 	}
 }
 
@@ -38,4 +50,6 @@ func (a *App) Stop() {
 	ctx, cancel := context.WithTimeout(context.Background(), a.cfg.RestConf.ShutdownTimeout)
 	defer cancel()
 	a.serv.Stop(ctx)
+
+	a.db.Close()
 }
