2f31e79f79654b80b5841536871f6b8380078d1b
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-31 08:17:48 +0300

    feat: added new querie

--- projectservice/internal/infrastructure/postgres/queries.go
+++ projectservice/internal/infrastructure/postgres/queries.go
@@ -3,4 +3,5 @@ package postgres
 var (
 	QuerieSave   = "INSERT INTO projects(owner_id, name) VALUES($1, $2)"
 	QuerieDelete = "DELETE FROM projects WHERE owner_id = $1 AND name = $2"
+	QuerieGetAll = "SELECT id, owner_id, name, created_at FROM projects WHERE owner_id = $1"
 )
