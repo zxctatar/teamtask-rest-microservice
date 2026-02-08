1bf7dece63e4baf9ad0cd2f0ece163457143b734
ashlyaud <maksimknutov90@gmail.com>
2026-02-08 04:24:38 +0300

    chore: added new error ErrInvalidProjectId

--- projectservice/internal/usecase/error/deleteproject/error.go
+++ projectservice/internal/usecase/error/deleteproject/error.go
@@ -3,5 +3,6 @@ package deleteerr
 import "errors"
 
 var (
-	ErrProjectNotFound = errors.New("project not found")
+	ErrProjectNotFound  = errors.New("project not found")
+	ErrInvalidProjectId = errors.New("invalid project id")
 )
